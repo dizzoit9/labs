3fa4f5065adb6c65c003255e424cfd80ab3847f9
dzarembo9 <49951326+dzarembo9@users.noreply.github.com>
2019-04-24 21:15:48 +0300

    A few changes in Program.cs

--- Program.cs
+++ Program.cs
@@ -8,13 +8,10 @@ namespace labs
         {
             Console.WriteLine("Hello World!");
 	    Console.ReadLine();
-<<<<<<< HEAD
 	    int a = 2;
 	    Console.WriteLine(a);
 	    string str = "hello";
 	    Console.WriteLine(str);
-=======
->>>>>>> parent of 0a71615... Fix some bugs in Program.cs
         }
     }
 }
