6820454c6fee78484ce5ad3ddbfd404ebd520b8a
dzarembo9 <49951326+dzarembo9@users.noreply.github.com>
2019-04-24 21:25:05 +0300

    Revert "Revert "Revert "A few changes in Program.cs"""
    This reverts commit 85006b24532098b56ea8f4f0c4031c8443519875.

--- Program.cs
+++ Program.cs
@@ -8,10 +8,13 @@ namespace labs
         {
             Console.WriteLine("Hello World!");
 	    Console.ReadLine();
+<<<<<<< HEAD
 	    int a = 2;
 	    Console.WriteLine(a);
 	    string str = "hello";
 	    Console.WriteLine(str);
+=======
+>>>>>>> parent of 0a71615... Fix some bugs in Program.cs
         }
     }
 }
